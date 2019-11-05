ca5c4679355a36ee274e1700c05271fbc3ae0289
SACHINKUMARHACKER <sachinkum123567@gmail.com>
2019-11-06 01:46:09 +0530

    first commit obviously

---
+++ demo.py
@@ -0,0 +1,13 @@
+#%%
+# importing the required libraries
+import numpy as np 
+import pandas as pd 
+import seaborn as sns 
+from matplotlib import pyplot as plt 
+%matplotlib inline
+
+# %%
+print("first commit")
+print(np.__version__)
+
+# %%
